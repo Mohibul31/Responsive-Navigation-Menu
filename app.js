a75af75849f2a9260bc9b7a5700942315cb8596c
Mohibul Islam <146790203+Mohibul31@users.noreply.github.com>
2024-10-08 16:25:26 +0600

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ function show() {
 function hide() {
     sidebar.style.display = 'none'
 }
+
+// For Scroll to Top Button
+const btn = document.getElementById("btn");
+
+//hide and show btn
+window.onscroll = function  () {
+    hideBtn();
+}
+
+function hideBtn () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btn.style.display = 'block'
+    }else{
+        btn.style.display = 'none'
+    }
+}
+
+//for scrolling
+btn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+})
